36938398856ffbe9590f42120b51f96e32f966fc
Chris Puppe <chrispuppe@gmail.com>
2021-12-18 14:46:13 -0800

    Part 2 Done

--- about.js
+++ about.js
@@ -1,14 +1,20 @@
-console.log("hello world");
-
-
+// console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	// console.log('form submit');
+	window.alert('Your form has been submitted successfully.')
 }
-
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+function handleDuckMouseOver() {
+	window.alert("You're so smart.")
+}
+
+let duck = document.getElementById('rubber-duck')
+
+duck.addEventListener('mouseover', handleDuckMouseOver)
